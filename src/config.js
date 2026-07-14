4d8cc486c41d056bfd4221c60e181e2128ee2248
Christian Narcia <57084243+Christian-Narcia@users.noreply.github.com>
2026-07-14 00:40:57 -0500

    version check

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@
  */
 
 /** Tool version, shown in the panel footer. Bump on release. */
-export const APP_VERSION = '0.1.102';
+export const APP_VERSION = '0.1.103';
 
 /** Detection parameters (also the initial slider values). */
 export const DEFAULT_PARAMS = Object.freeze({
